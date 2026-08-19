5f2d3c9196806b54cd23ea1a4b8bac5b731d67da
Beatriz Frohlich <beatriz_frohlich@estudante.sesisenai.org.br>
2026-08-19 11:20:23 -0300

    up: atualizei index

--- index.php
+++ index.php
@@ -6,19 +6,26 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sistema do Restaurante</title>
+    <link rel="stylesheet" href="css/style.css">
 
 </head>
 
 <body>
+    <div class="container">
+        <header>
+            <h1>Sistema do Restaurante</h1>
+            <p>Gerenciamento de usuários e pratos</p>
+        </header>
 
-    <h1>Sistema do Restaurante</h1>
-
-    <h2>Menu</h2>
-
-    <p><a href="usuario.php"> Cadastrar Usuário</a></p>
-    <p><a href="cadastrar_prato.php">Cadastrar Prato</a></p>
-    <p><a href="pratos.php">Visualizar Pratos</a></p>
-
+        <main>
+            <h2>Menu principal</h2>
+            <div class="menu">
+                <a href="usuario.php">Cadastrar Usuário</a>
+                <a href="cadastrar_prato.php">Cadastrar Prato</a>
+                <a href="pratos.php">Visualizar Pratos</a>
+            </div>
+        </main>
+    </div>
 </body>
 
 </html>
